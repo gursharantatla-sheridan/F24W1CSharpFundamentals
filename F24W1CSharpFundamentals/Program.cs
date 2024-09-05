eb447b3a175ba4b3f6460e5213f62d1ab3afb7d3
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-05 13:41:47 -0400

    variables

--- F24W1CSharpFundamentals/Program.cs
+++ F24W1CSharpFundamentals/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        static int y;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int @float;
+            int x;
+
+            //Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            double d = 5.5;
+            float f = 5.5F;
+            decimal m = 5.5M;
+            long l = 45433432L;
+
+            var p = "5.5";
+
+            Console.WriteLine(@"hello\nworld");
+
+
+            int cars = 4, bikes = 7, trucks = 8;
+
+            // string concatenation
+            Console.WriteLine("I have " + cars +" cars, " + bikes + " bikes and " + trucks + " trucks");
+
+            // string interpolation
+            Console.WriteLine($"I have {cars} cars, {bikes} bikes and {trucks} trucks");
         }
     }
 }
